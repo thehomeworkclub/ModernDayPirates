01b6d064ec29249d3b41e372872c58635f915e4a
SebastianAlexis <sebastianralexis@gmail.com>
2025-03-24 19:00:46 -0700

    added shop upgrade levels (basic)

--- shop1.gd
+++ shop1.gd
@@ -14,11 +14,22 @@ var right_ray: RayCast3D = null
 var left_ray: RayCast3D = null
 var right_laser_dot: MeshInstance3D = null
 var left_laser_dot: MeshInstance3D = null
-var ray_length = 10.0
 var normal_ray_color = Color(0, 0.5, 1, 0.6)  # Blue
 var hover_ray_color = Color(0, 1, 0, 0.6)     # Green
 
 var current_shop = "bronze"
+
+# Upgrade tracking
+var item_levels = {
+	"ItemBox1": 1,
+	"ItemBox2": 1,
+	"ItemBox3": 1,
+	"ItemBox4": 1,
+	"ItemBox5": 1,
+	"ItemBox6": 1
+}
+
+var max_level = 10  # Maximum upgrade level
 
 func _ready():
 	print("\n=== Shop Menu Initializing ===")
@@ -32,62 +43,37 @@ func _ready():
 		right_controller.button_pressed.connect(_on_button_pressed.bind("right"))
 		right_ray = right_controller.get_node("RayCastRight")
 		right_laser_dot = right_controller.get_node("LaserDotRight")
-		
-		if right_ray:
-			print("Found right raycast")
-			right_ray.target_position = Vector3(0, 0, -ray_length)
-			print("Right ray collision mask: " + str(right_ray.collision_mask))
-			
-		# Initialize right laser material
-		var right_laser = right_controller.get_node("LaserBeamRight")
-		if right_laser:
-			var material = StandardMaterial3D.new()
-			material.flags_transparent = true
-			material.albedo_color = normal_ray_color
-			material.emission_enabled = true
-			material.emission = normal_ray_color
-			material.emission_energy = 2.0
-			right_laser.material_override = material
-			print("Initialized right laser material")
 
 	if left_controller:
 		print("Found left controller")
 		left_controller.button_pressed.connect(_on_button_pressed.bind("left"))
 		left_ray = left_controller.get_node("RayCastLeft")
 		left_laser_dot = left_controller.get_node("LaserDotLeft")
-		
-		if left_ray:
-			print("Found left raycast")
-			left_ray.target_position = Vector3(0, 0, -ray_length)
-			print("Left ray collision mask: " + str(left_ray.collision_mask))
-			
-		# Initialize left laser material
-		var left_laser = left_controller.get_node("LaserBeamLeft")
-		if left_laser:
-			var material = StandardMaterial3D.new()
-			material.flags_transparent = true
-			material.albedo_color = normal_ray_color
-			material.emission_enabled = true
-			material.emission = normal_ray_color
-			material.emission_energy = 2.0
-			left_laser.material_override = material
-			print("Initialized left laser material")
 			
 	# Initialize shop state
 	change_shop("bronze")
+	
+	# Update all item labels
+	update_all_item_labels()
 
 func _on_button_pressed(button_name: String, controller: String):
+	print("\nController Button Event: " + button_name)
+	
 	if button_name != "trigger_click":
 		return
 		
+	print(controller + " trigger clicked")
 	var raycast = get_node("XROrigin3D/XRController3D" + controller.capitalize() + "/RayCast" + controller.capitalize())
 	if !raycast.is_colliding():
 		return
 		
 	var collider = raycast.get_collider()
+	print(controller + " ray hit: " + str(collider.name if collider else "None"))
+	
 	if !collider or !collider.is_in_group("button"):
 		return
 		
+	print("Pressing button: " + collider.name)
 	handle_button_press(collider)
 
 func handle_button_press(button: Node):
@@ -104,7 +90,23 @@ func handle_button_press(button: Node):
 				change_shop("gold")
 		emit_signal("tab_selected", button_name)
 	elif parent_name == "ItemBoundingBoxes":
+		upgrade_item(button_name)
 		emit_signal("item_selected", button_name)
+
+func upgrade_item(item_name: String):
+	if item_levels[item_name] < max_level:
+		item_levels[item_name] += 1
+		update_item_label(item_name)
+		print("Upgraded " + item_name + " to level " + str(item_levels[item_name]))
+
+func update_item_label(item_name: String):
+	var label = get_node("ItemBoundingBoxes/" + item_name + "/Label3D")
+	if label:
+		label.text = str(item_levels[item_name])
+
+func update_all_item_labels():
+	for item_name in item_levels.keys():
+		update_item_label(item_name)
 
 func change_shop(shop_type: String):
 	current_shop = shop_type
@@ -130,47 +132,43 @@ func update_laser_pointers():
 	# Update right controller laser
 	if right_ray and right_laser_dot:
 		var right_laser = right_controller.get_node("LaserBeamRight")
-		if right_laser:
-			var material = right_laser.material_override
-			if material:
-				if right_ray.is_colliding():
-					var collision_point = right_ray.get_collision_point()
-					var collider = right_ray.get_collider()
-					
-					right_laser_dot.global_position = collision_point
-					right_laser_dot.visible = true
-					
-					if collider and collider.is_in_group("button"):
-						material.albedo_color = hover_ray_color
-						material.emission = hover_ray_color
-					else:
-						material.albedo_color = normal_ray_color
-						material.emission = normal_ray_color
+		if right_laser and right_laser.material_override:
+			if right_ray.is_colliding():
+				var collision_point = right_ray.get_collision_point()
+				var collider = right_ray.get_collider()
+				
+				right_laser_dot.global_position = collision_point
+				right_laser_dot.visible = true
+				
+				if collider and collider.is_in_group("button"):
+					right_laser.material_override.albedo_color = hover_ray_color
+					right_laser.material_override.emission = hover_ray_color
 				else:
-					material.albedo_color = normal_ray_color
-					material.emission = normal_ray_color
-					right_laser_dot.visible = false
+					right_laser.material_override.albedo_color = normal_ray_color
+					right_laser.material_override.emission = normal_ray_color
+			else:
+				right_laser.material_override.albedo_color = normal_ray_color
+				right_laser.material_override.emission = normal_ray_color
+				right_laser_dot.visible = false
 
 	# Update left controller laser
 	if left_ray and left_laser_dot:
 		var left_laser = left_controller.get_node("LaserBeamLeft")
-		if left_laser:
-			var material = left_laser.material_override
-			if material:
-				if left_ray.is_colliding():
-					var collision_point = left_ray.get_collision_point()
-					var collider = left_ray.get_collider()
-					
-					left_laser_dot.global_position = collision_point
-					left_laser_dot.visible = true
-					
-					if collider and collider.is_in_group("button"):
-						material.albedo_color = hover_ray_color
-						material.emission = hover_ray_color
-					else:
-						material.albedo_color = normal_ray_color
-						material.emission = normal_ray_color
+		if left_laser and left_laser.material_override:
+			if left_ray.is_colliding():
+				var collision_point = left_ray.get_collision_point()
+				var collider = left_ray.get_collider()
+				
+				left_laser_dot.global_position = collision_point
+				left_laser_dot.visible = true
+				
+				if collider and collider.is_in_group("button"):
+					left_laser.material_override.albedo_color = hover_ray_color
+					left_laser.material_override.emission = hover_ray_color
 				else:
-					material.albedo_color = normal_ray_color
-					material.emission = normal_ray_color
-					left_laser_dot.visible = false
+					left_laser.material_override.albedo_color = normal_ray_color
+					left_laser.material_override.emission = normal_ray_color
+			else:
+				left_laser.material_override.albedo_color = normal_ray_color
+				left_laser.material_override.emission = normal_ray_color
+				left_laser_dot.visible = false
